30323f5dd9c491452fa0e59cfd72797935003cb0
Mauricio <zuluaga424@gmail.com>
2025-04-30 19:35:22 -0500

    first commit with code changes

---
+++ WebAppVeterinaria/Models/Persona.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAppVeterinaria.Models
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Cedula { get; set; }
+        public string Telefono { get; set; }
+
+    }
+}
